8771ce224570387ab8da5facb5bf039074d90e26
Artem Lubkivskyi <artemlyb15@gmail.com>
2022-05-15 18:40:48 +0200

    first commit

---
+++ oop1no1.py
@@ -0,0 +1,11 @@
+class Book:
+
+    def __init__(self, author, year, genre):
+        self.author = author
+        self.year = year
+        self.genre = genre
+
+
+voinaimir = Book(Tolsoi, 1869, novel)
+crimeandpunishment = Book(Dostoevskyi, 1866, novel)
+kolobok = Book(nation, idk, fairy_tale)
